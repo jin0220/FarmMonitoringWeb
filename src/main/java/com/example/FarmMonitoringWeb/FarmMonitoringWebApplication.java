0ad60efb6b6d9727ae72dd9bdbf58b8b07d3f7be
jin0220 <37447282+jin0220@users.noreply.github.com>
2021-07-13 17:01:59 +0900

    Initial commit

---
+++ src/main/java/com/example/FarmMonitoringWeb/FarmMonitoringWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.FarmMonitoringWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmMonitoringWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmMonitoringWebApplication.class, args);
+	}
+
+}
