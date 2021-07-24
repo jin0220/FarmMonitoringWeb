7d40ac37e47b03f07aa6c11c6f7509eca30225be
jin0220 <37447282+jin0220@users.noreply.github.com>
2021-07-24 18:55:55 +0900

    commit
    페이지 추가

--- src/main/java/com/example/FarmMonitoringWeb/controller/MainController.java
+++ src/main/java/com/example/FarmMonitoringWeb/controller/MainController.java
@@ -19,4 +19,39 @@ public class MainController {
     public String signup(){
         return "signup";
     }
+
+    @GetMapping("information")
+    public String information(){
+        return "content/information";
+    }
+
+    @GetMapping("editor")
+    public String editor(){
+        return "content/editor";
+    }
+
+    @GetMapping("community")
+    public String community(){
+        return "content/community";
+    }
+
+    @GetMapping("notice")
+    public String notice(){
+        return "content/notice";
+    }
+
+    @GetMapping("guidely")
+    public String guidely(){
+        return "content/guidely";
+    }
+
+    @GetMapping("faq")
+    public String faq(){
+        return "content/faq";
+    }
+
+    @GetMapping("qna")
+    public String qna(){
+        return "content/qna";
+    }
 }
