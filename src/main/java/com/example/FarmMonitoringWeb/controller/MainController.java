40366281cd728c409c8aad94772b2aff49d97456
jin0220 <37447282+jin0220@users.noreply.github.com>
2021-07-27 17:26:52 +0900

    commit

--- src/main/java/com/example/FarmMonitoringWeb/controller/MainController.java
+++ src/main/java/com/example/FarmMonitoringWeb/controller/MainController.java
@@ -1,12 +1,14 @@
 package com.example.FarmMonitoringWeb.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
     @GetMapping("/")
-    public String home(){
+    public String home(Model model){
+        model.addAttribute("category",1);
         return "content/home";
     }
 
@@ -21,37 +23,44 @@ public class MainController {
     }
 
     @GetMapping("information")
-    public String information(){
+    public String information(Model model){
+        model.addAttribute("category",3);
         return "content/information";
     }
 
     @GetMapping("editor")
-    public String editor(){
+    public String editor(Model model){
+        model.addAttribute("category",2);
         return "content/editor";
     }
 
     @GetMapping("community")
-    public String community(){
+    public String community(Model model){
+        model.addAttribute("category",2);
         return "content/community";
     }
 
     @GetMapping("notice")
-    public String notice(){
+    public String notice(Model model){
+        model.addAttribute("category",4);
         return "content/notice";
     }
 
     @GetMapping("guidely")
-    public String guidely(){
+    public String guidely(Model model){
+        model.addAttribute("category",4);
         return "content/guidely";
     }
 
     @GetMapping("faq")
-    public String faq(){
+    public String faq(Model model){
+        model.addAttribute("category",4);
         return "content/faq";
     }
 
     @GetMapping("qna")
-    public String qna(){
+    public String qna(Model model){
+        model.addAttribute("category",4);
         return "content/qna";
     }
 }
